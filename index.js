1d076c7c8d3504625cf2edd325664e56b31574b2
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 17:43:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function writeCards( namesArray, event ) {
   let thankYouCards = []
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < namesArray.length; i++ ) {
     console.log(`Thank you ${cards[i]} for the wonderful birthday gift!`);
   }
  
