6ebec57d56680275781d0961cdf68b6ed1162588
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 17:41:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-const writeCards =["Ada", "Brendan", "Ali"];
+const writeCards = ["Ada", "Brendan", "Ali"];
  
 function writeCards(cards) {
   for (let i = 0; i < cards.length; i++) {
