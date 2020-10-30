bba4d43a735b1239c10b950ed62b0d4cb3665744
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 17:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solutions in this file
-const writeCards = ["Ada", "Brendan", "Ali"];
- 
-function writeCards(cards) {
+function writeCards() {
   for (let i = 0; i < cards.length; i++) {
     console.log(`Thank you ${cards[i]} for the wonderful birthday gift!`);
   }
