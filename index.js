286441342037c0d5be3402c7de61b59ce8aea88e
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 17:54:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function writeCards( namesArray, event ) {
   return thankYouCards
 }
 
+function countDown( startingNumber) {
+  while ( startingNumber > 0 ){
+    console.log( startingNumber );
+    startingNumber -= 1; 
+  }
+  console.log( startingNumber )
+}
